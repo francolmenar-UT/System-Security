75db220e73d14d4b7b685e5ff1369baeb8059158
draane <andrea@dallacosta.me>
2020-05-15 22:02:05 +0200

    Refractor and add log to file

--- PUF/src/config.hpp
+++ PUF/src/config.hpp
@@ -1,6 +1,8 @@
 #define N 5
-#define SIMULATIONS 1
+#define SIMULATIONS 20
 
 // Delay values
-#define T0 {1, 10, 1, 1, 1}
-#define T1 {1, 1, 1, 1, 1}
+#define T0 {7, 10, 3, 7, 12}
+#define T1 {8, 9, 2, 4, 13}
+
+//#define DEBUG
