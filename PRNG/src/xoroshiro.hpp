61da1cb23da785c5625a63f269181ada275e77ef
francolmenar <francolmenar@outlook.es>
2020-05-15 18:27:24 +0200

    Code commented and checked. Another approach is added to be considered

--- PRNG/src/xoroshiro.hpp
+++ PRNG/src/xoroshiro.hpp
@@ -4,10 +4,10 @@
 #include <random>
 #include <array>
 
-/**** Parameters used in the Algorithm. Defined by the specification ***/
-extern int gen_a = 24;
-extern int gen_b = 16;
-extern int gen_c = 37;
+/**** Parameters used in the Algorithm. Defined by the specification ****/
+extern const int gen_a;
+extern const int gen_b;
+extern const int gen_c;
 
 struct xoroshiro128plus_gen {
 private:
@@ -15,18 +15,23 @@ private:
     uint64_t state[2];
 
     /**
-     * Function to rotate to the left the "x" variable by "k"
+     * It performs a Left sift of x by k OR Right sift of x by (k - 1)
      *
      * @param x: uint64_t to be left rotated, it will be state[0] or state[1]
      * @param k: input parameter to operate with (gen_a, gen_b or gen_c)
      */
     static inline uint64_t rotl(const uint64_t x, int k) {
+        // Left sift of x by k OR Right sift of x by (k - 1)
         return (x << k) | (x >> (64 - k));
     }
 
 
 public:
     using result_type = uint64_t;
+
+    constexpr static result_type min() { return 0; }
+
+    constexpr static result_type max() { return -1; }
 
     result_type operator()();
 
