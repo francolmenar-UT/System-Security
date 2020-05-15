31253089824b84867b61bcb400480d414299b6da
francolmenar <francolmenar@outlook.es>
2020-05-15 18:36:21 +0200

    Second approach cleaned

--- PRNG/src/other_approach/example.cc
+++ PRNG/src/other_approach/example.cc
@@ -8,56 +8,18 @@
 
 namespace xs = xorshift;
 
-template <typename T>
-std::ostream& operator<<(std::ostream& os, std::vector<T> vec) {
-  os << "[ ";
-  if (vec.size() != 0) {
-    std::copy(vec.begin(), vec.end() - 1, std::ostream_iterator<T>(os, ", "));
-    os << vec.back();
-  }
-  os << " ]";
-  return os;
-}
-
 int main() {
-  auto now = [](){
-    return std::chrono::system_clock::now().time_since_epoch().count();
-  };
+    auto now = []() {
+        return std::chrono::system_clock::now().time_since_epoch().count();
+    };
 
-  // splitmix64
-  xs::SplitMix64 sm64(now());
-  std::cout << "SplitMix64: " << sm64.Next() << std::endl;
+    // xoroshiro128
+    std::array<uint64_t, 2> state1;
+    state1[0] = now();
+    state1[1] = now();
+    xs::Xoroshiro128 xoro(state1);
+    std::vector<uint64_t> v(20);
+    std::cout << "Xoroshiro128: " << xoro.Next() << std::endl;
 
-  // xorshift128
-  std::array<uint64_t, xs::Xorshift128::STATE_SIZE> state2;
-  std::generate(state2.begin(), state2.end(), now);
-  xs::Xorshift128 xs128(state2);
-  std::cout << "Xorshift128: " << xs128.Next() << std::endl;
-
-  // xoroshiro128
-  std::array<uint64_t, 2> state1;
-  state1[0] = now();
-  state1[1] = now();
-  xs::Xoroshiro128 xoro(state1);
-  std::vector<uint64_t> v(20);
-  std::cout << "Xoroshiro128: " << xoro.Next() << std::endl;
-
-  // seed splitmix64 with timestamp and then seed xorshift1024 with splitmix64
-  auto seed = now();
-  auto xs1024 = xs::SeedWithSm64<xs::Xorshift1024>(seed);
-  std::vector<uint64_t> vals(20);
-  std::generate(vals.begin(), vals.end(),
-                [xs1024]() mutable {return xs1024.Next();});
-  std::cout << "Xorshift1024: " << vals << std::endl;
-
-  // seed splitmix64 with same timestamp and then seed xorshift1024 and jump
-  auto xs1024_2 = xs::SeedWithSm64<xs::Xorshift1024>(seed);
-  xs1024_2.Jump();
-  std::generate(vals.begin(), vals.end(),
-                [xs1024_2]() mutable {return xs1024_2.Next();});
-  std::cout << "Xorshift1024_Jumped: " << vals << std::endl;
-
-
-
-  return 0;
+    return 0;
 }
