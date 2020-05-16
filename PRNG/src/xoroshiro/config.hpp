325a9628c4f092e316aaab5ea5e9e4a56c79d944
francolmenar <francolmenar@outlook.es>
2020-05-16 18:04:13 +0200

    Test1 handle done

--- PRNG/src/xoroshiro/config.hpp
+++ PRNG/src/xoroshiro/config.hpp
@@ -1,7 +1,7 @@
-#define OUTPUT_INSTANCES 1 // Amount of random numbers generated
+#define OUTPUT_INSTANCES 5 // Amount of random numbers generated
 #define BLOCKS 3  // Blocks of
 #define BLOCK_LENGTH 50 // Length of the bit string output
-#define TOTAL_BIT_LENGTH 150 // Length of the bit string output
+#define TOTAL_BIT_LENGTH 150 // Length of the bit string output  - Change also in python!!!
 
 // Parameters used in the Algorithm. Defined by the specification
 #define GEN_A 24
