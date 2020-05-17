94f4ad34dd049ccaad99d97454d3c5205f09b25c
francolmenar <francolmenar@outlook.es>
2020-05-17 11:23:00 +0200

    Analysis files copied, Special tests executed separately

--- PRNG/src/python/constants.py
+++ PRNG/src/python/constants.py
@@ -2,18 +2,21 @@
 test_path = "../../sts-2.1.2/"
 test_path_c = "../sts-2.1.2/" # From the c Program
 program_path = "../../cmake-build-debug/"
-output_path = "output/"
-output_path_test = "../output/"  # From the executable of tests
+output_path = "output/bit_streams/"
+output_path_test = "../output/bit_streams/"  # From the executable of tests
 test_obj = "obj/"
+report_path = "sts-2.1.2/experiments/AlgorithmTesting/finalAnalysisReport.txt"  # Path to the resulting report
+results_path = "output/analysis/"
 
 # Programs path
 c_program = "prng"
 test_program = "./assess"
 
-# Files path
+# Files Name
 output_file = "output.txt"
 test10_file = "test10.txt"
 test12_13_file = "test12.txt"
+analysis = "finalAnalysisReport"
 
 # Variables for test
 total_bit_length = 10000
@@ -28,6 +31,7 @@ exec_test_10 = ["0", "0", "0", "0", "0",  # Special Execution for test 10
                 "0", "0", "0", "0", "1",
                 "0", "0", "0", "0", "0"]
 
+exe_10 = "000000000100000"
 bit_streams_test_10 = 5
 total_bit_length_test_10 = 5242880
 
@@ -36,6 +40,7 @@ exec_test_12 = ["0", "0", "0", "0", "0",  # Special Execution for test 12
                 "0", "0", "0", "0", "0",
                 "0", "1", "0", "0", "0"]
 
+exe_12 = "000000000001000"
 bit_streams_test_12_13 = 5
 total_bit_length_test_12_13 = 1000000
 
@@ -44,10 +49,15 @@ exec_test_13 = ["0", "0", "0", "0", "0",  # Special Execution for test 13
                 "0", "0", "0", "0", "0",
                 "0", "0", "1", "0", "0"]
 
+exe_13 = "000000000000100"
+
 # Variables for Test 12 & 13
 exec_test_12_13 = ["0", "0", "0", "0", "0",  # Special Execution for test 12 & 13
                    "0", "0", "0", "0", "0",
                    "0", "1", "1", "0", "0"]
+
+exe_12_13 = "000000000001100"
+
 
 # The sixth requires 32x32 bits -> Handled by increasing the size
 # The 10th is different -> Handled special case created
--- PRNG/src/python/test_handle.py
+++ PRNG/src/python/test_handle.py
@@ -11,6 +11,22 @@ def test_handle(test_list):
     """
 
     is_additional = ""  # Check if there is any input which needs additional parameters
+    output_name = analysis  # Used for copying the files and setting the right name
+    is_test_10 = False
+    is_test_12 = False
+    is_test_13 = False
+
+    if 10 in test_list:
+        is_test_10 = True
+        test_list.remove(10)
+
+    if 12 in test_list:
+        is_test_12 = True
+        test_list.remove(12)
+
+    if 13 in test_list:
+        is_test_13 = True
+        test_list.remove(13)
 
     # Update the String of Executing tests
     for i, item in enumerate(test_list):
@@ -20,17 +36,28 @@ def test_handle(test_list):
         if additional_par[int(item) - 1] is True:
             is_additional = "0"
 
+    if len(test_list) > 0:  # Run the normal tests
+        run_tests(is_additional)
+
+    # The rest of tests does not need the additional arguments
+    is_additional = ""
+
     # Special execution for test 10
-    if exec_string == exec_test_10:
-        run_test_10(is_additional)
-        return
-    
+    if is_test_10:
+        run_test_10(is_additional, exe_10, output_name + "_10.txt")
+
     # Special execution for test 12 & 13
-    if exec_string == exec_test_12 or exec_string == exec_test_13 or exec_string == exec_test_12_13:
-        run_test_12_13(is_additional)
+    if is_test_12 and is_test_13:
+        run_test_12_13(is_additional, exe_12_13, output_name + "_12-13.txt")
         return
-    
-    run_tests(is_additional)
+
+    elif is_test_12:
+        run_test_12_13(is_additional, exe_12, output_name + "_12.txt")
+
+    elif is_test_13:
+        run_test_12_13(is_additional, exe_13, output_name + "_13.txt")
+
+    return
 
 
 def run_tests(is_additional):
@@ -48,8 +75,24 @@ def run_tests(is_additional):
                                file_format,
                                test_program, total_bit_length))
 
+    os.system("cp ../{} ../{}".format(report_path, results_path))
 
-def run_test_10(is_additional):
+
+def run_test_10(is_additional, exe_10, output_name):
+    print("echo '0' "  # Select option input file
+          "{}{} "  # Path to the output file
+          "0 "  # Choose the tests
+          "{} "  # Execution String of tests
+          "{} "  # Additional parameters
+          "{} "  # Amount of bit-streams
+          "{} "  # ASCII [0] or Binary [1]
+          "| {} {}".format(output_path_test, test10_file,
+                           exe_10,
+                           is_additional,
+                           bit_streams_test_10,
+                           file_format,
+                           test_program, total_bit_length_test_10))
+
     os.system("echo '0' "  # Select option input file
               "{}{} "  # Path to the output file
               "0 "  # Choose the tests
@@ -58,14 +101,16 @@ def run_test_10(is_additional):
               "{} "  # Amount of bit-streams
               "{} "  # ASCII [0] or Binary [1]
               "| {} {}".format(output_path_test, test10_file,
-                               ''.join(exec_string),
+                               exe_10,
                                is_additional,
                                bit_streams_test_10,
                                file_format,
                                test_program, total_bit_length_test_10))
 
+    os.system("cp ../{} ../{}{}".format(report_path, results_path, output_name))
 
-def run_test_12_13(is_additional):
+
+def run_test_12_13(is_additional, exe_12_13, output_name):
     os.system("echo '0' "  # Select option input file
               "{}{} "  # Path to the output file
               "0 "  # Choose the tests
@@ -74,8 +119,10 @@ def run_test_12_13(is_additional):
               "{} "  # Amount of bit-streams
               "{} "  # ASCII [0] or Binary [1]
               "| {} {}".format(output_path_test, test12_13_file,
-                               ''.join(exec_string),
+                               exe_12_13,
                                is_additional,
                                bit_streams_test_12_13,
                                file_format,
                                test_program, total_bit_length_test_12_13))
+
+    os.system("cp ../{} ../{}{}".format(report_path, results_path, output_name))
