fdc20a9fd682edebb15b528188ae152c95f1b3b3
francolmenar <francolmenar@outlook.es>
2020-05-18 20:06:27 +0200

    cmake added to be able to run it directly

---
+++ PRNG/cmake-build-debug/CMakeFiles/prng.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fran/Documents/Estudios/Master/1º-Master/Q4/System-Security/System-Security/PRNG/src/xoroshiro/main.cpp" "/Users/fran/Documents/Estudios/Master/1º-Master/Q4/System-Security/System-Security/PRNG/cmake-build-debug/CMakeFiles/prng.dir/src/xoroshiro/main.cpp.o"
+  "/Users/fran/Documents/Estudios/Master/1º-Master/Q4/System-Security/System-Security/PRNG/src/xoroshiro/xoroshiro.cpp" "/Users/fran/Documents/Estudios/Master/1º-Master/Q4/System-Security/System-Security/PRNG/cmake-build-debug/CMakeFiles/prng.dir/src/xoroshiro/xoroshiro.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
