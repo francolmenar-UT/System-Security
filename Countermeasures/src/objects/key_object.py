c4e68c9a921a6f900aacc3786398ffbd553b20e6
francolmenar <francolmenar@outlook.es>
2020-05-27 18:43:12 +0200

    Crypto cleaned, constants on the way

--- Countermeasures/src/objects/key_object.py
+++ Countermeasures/src/objects/key_object.py
@@ -1,40 +1,53 @@
+from random import randrange
+
+
 class KeyObject:
+    e = None
+    n = None
+    msg = None
+    enc_time = None
 
-    def __init__(self, key=None, e=None, d=None, n=None, key_size=None):
-        self.e = None
-        self.d = None
-        self.n = None
-        self.key_size = None
-        if key is not None:
-            self.create_from_key(key, key_size)
+    def __init__(self, e=None, n=None, msg=None, enc_time=None):
+        if enc_time is not None:
+            self.create_from_time(e, n, msg, enc_time)
         else:
-            self.create_from_values(e, d, n, key_size)
+            self.create_from_values(e, n, msg)
 
-    def create_from_key(self, key, key_size):
-        """
-        Used when it is called from crypto
-        :param key:
-        :param key_size:
-        :return:
-        """
-        self.e = key.public_key().public_numbers().e
-        self.d = key.private_numbers().d
-        self.n = key.public_key().public_numbers().n
-        self.key_size = key_size
-
-    def create_from_values(self, e, d, n, key_size):
+    def create_from_values(self, e, n, msg):
         """
         Used when it is called from loading the keys
+        :param msg:
         :param e:
-        :param d:
         :param n:
-        :param key_size:
         :return:
         """
         self.e = e
-        self.d = d
         self.n = n
-        self.key_size = key_size
+        self.msg = msg
+
+    def create_from_time(self, e, n, msg, enc_time):
+        """
+        # TODO
+        :param e:
+        :param n:
+        :param msg:
+        :param enc_time:
+        :return:
+        """
+        self.e = e
+        self.n = n
+        self.msg = msg
+        self.enc_time = enc_time
+
+    def add_time(self, new_enc_time):
+        """
+        # TODO
+        :param new_enc_time:
+        :return:
+        """
+        return KeyObject(e=self.e, n=self.n,
+                         msg=self.msg, enc_time=new_enc_time)
 
     def toString(self):
-        print("e: {}\n d: {}\n n: {}\n key_size: {}".format(self.e, self.d, self.n, self.key_size))
+        print(" e: {}\n n: {}\n  msg:{}\n enc_time: {}"
+              .format(self.e, self.n, self.msg, self.enc_time))
