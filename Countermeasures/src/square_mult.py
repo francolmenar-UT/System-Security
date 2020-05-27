1e27782c25758eaebe6ddab1767e35b628faf213
francolmenar <francolmenar@outlook.es>
2020-05-27 10:04:10 +0200

    square and multiply done

--- Countermeasures/src/square_mult.py
+++ Countermeasures/src/square_mult.py
@@ -1,10 +1,23 @@
 # Code copied  - Review
-def exp_func(x, y):
-    exp = bin(y)
-    value = x
+def square_mult(m, d, n):
+    m = m % n  # Calculate the modulus at first
+    d_bin = bin(d)
+    d_bin = d_bin[:0] + d_bin[0 + 2:]  # Remove first two characters to have a bit array
 
-    for i in range(3, len(exp)):
-        value = value * value
-        if (exp[i:i + 1] == '1'):
-            value = value * x
-    return value
+    a = 1  # a <- 1
+
+    # print("d_bin: {}".format(d_bin))
+    # print("len d_bin: {}".format(len(d_bin)))
+
+    # for i in reversed(range(0, len(d_bin))):
+        # print("d_bin[{}]: {}".format(i, d_bin[i]))
+
+    # print("Innnn")
+
+    for i in range(0, len(d_bin) ):
+        # print("d_bin[{}]: {}".format(i, d_bin[i]))
+
+        a = (a * a) % n  # a <- a^2  mod n
+        if d_bin[i:i+1] == '1':
+            a = (a * m) % n  # a x m mod n
+    return a % n
